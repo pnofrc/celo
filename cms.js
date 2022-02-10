426dec55f88a1937a1c52b750c348dd65f37c114
funix <funix@145-137-31-179.wlan.hro.nl>
2022-02-10 18:01:43 +0100

    add js files

---
+++ cms.js
@@ -0,0 +1,91 @@
+var assets = [
+    [
+        ['Manuel Ritz'],
+        ['https://player.vimeo.com/video/352223101'],
+        ['Tutto sommato è un bel film']
+    ],
+    [
+        ['Simona Corsellini'],
+        ['https://player.vimeo.com/video/400114006'],
+        ['description']
+    ],
+    [
+        ['Roger Vivier'],
+        ['https://player.vimeo.com/video/481229528'],
+        ['description']
+    ],
+    [
+        ['Coltorti'],
+        ['https://player.vimeo.com/video/352222024'],
+        ['description']
+    ],
+    [
+        ['Department Five SS21'],
+        ['https://player.vimeo.com/video/535528039'],
+        ['description']
+    ],
+    [
+        ['Nike/Yoox'],
+        ['https://player.vimeo.com/video/496674956'],
+        ['description']
+    ],
+    [
+        ['Womsh'],
+        ['https://player.vimeo.com/video/569872956'],
+        ['description']
+    ],
+    [
+        ['Womsh2'],
+        ['https://player.vimeo.com/video/669113967?h=029ed1d7b4'],
+        ['description']
+    ],
+    [
+        ['Bio'],
+        ['https://player.vimeo.com/video/669113967?h=029ed1d7b4'],
+        ['description']
+    ]
+]
+
+let titles = document.querySelectorAll("#data a");
+titles.forEach(el => {
+    let currentTit = el.title;
+    // console.log(currentTit)
+    el.innerHTML = assets[currentTit][0]
+    console.log()
+
+});
+
+
+let array = document.querySelectorAll(".planet");
+array.forEach(element => {
+    element.addEventListener("click", function() {
+
+        let current = element.title;
+        document.querySelector("iframe").src = assets[current][1];
+        $("#player").fadeIn()
+        $("#antiplayer").fadeIn()
+
+        document.getAnimations().forEach(
+            function(animation) {
+                animation.playbackRate = 0;
+            });
+
+    })
+});
+
+document.getElementById("closePlayer").addEventListener('click', function() {
+    $("#player").fadeOut()
+    $("#antiplayer").fadeOut()
+    document.querySelector("iframe").src = '';
+
+
+    document.getAnimations().forEach(
+        function(animation) {
+            animation.playbackRate = 1;
+        }
+    );
+})
+
+document.getElementById("blackhole").addEventListener("click", function() {
+    alert("Tutti gli altri video qua")
+})
---
+++ theme.js
@@ -0,0 +1,54 @@
+if (localStorage.lenght === 0) {
+    localStorage.setItem('theme') == 'light'
+    document.getElementById("cometImg").classList.add('invert');
+    document.getElementById("blackholeImg").classList.add('invert');
+} else if (localStorage.getItem('theme') == 'dark') {
+    document.body.style.setProperty('--background', 'black');
+    document.body.style.setProperty('--orbits', 'rgb(123, 0, 255)');
+    document.body.style.setProperty('--selectedOrbit', 'violet');
+    document.body.style.setProperty('--sun', 'magenta');
+    document.body.style.setProperty('--toggle', 'white');
+    document.body.style.setProperty('--shadow', 'rgba(255, 255, 255, 0.514)');
+    document.body.style.setProperty(' --playerOn', 'rgba(255, 255, 255, 0.3)');
+    localStorage.setItem('theme', 'dark')
+    console.log("dark mode")
+    document.getElementById("cometImg").classList.remove('invert');
+    document.getElementById("blackholeImg").classList.remove('invert');
+}
+
+
+let toggle = document.getElementById("toggle");
+toggle.addEventListener("click", function() {
+
+    if (localStorage.getItem('theme') == 'light') {
+
+        document.body.style.setProperty('--background', 'black');
+        document.body.style.setProperty('--orbits', 'rgb(123, 0, 255)');
+        document.body.style.setProperty('--selectedOrbit', 'violet');
+        document.body.style.setProperty('--sun', 'magenta');
+        document.body.style.setProperty('--toggle', 'white');
+        document.body.style.setProperty('--shadow', 'rgba(255,255,255, 0.514)');
+        document.body.style.setProperty(' --playerOn', 'rgba(255,255,255, 0.3)');
+        document.getElementById("cometImg").classList.remove('invert');
+        document.getElementById("blackholeImg").classList.remove('invert');
+
+
+        localStorage.setItem('theme', 'dark')
+        console.log("dark mode")
+
+    } else {
+        document.body.style.setProperty('--background', 'white');
+        document.body.style.setProperty('--orbits', 'rgb(255,136,0)');
+        document.body.style.setProperty('--selectedOrbit', 'rgb(255,0,0)');
+        document.body.style.setProperty('--sun', 'rgb(255,255,0)');
+        document.body.style.setProperty('--toggle', 'black');
+        document.body.style.setProperty('--shadow', 'rgba(0,0,0, 0.514)');
+        document.body.style.setProperty(' --playerOn', 'rgba(0,0,0, 0.3)');
+        document.getElementById("cometImg").classList.add('invert');
+        document.getElementById("blackholeImg").classList.add('invert');
+
+
+        localStorage.setItem('theme', 'light')
+        console.log("light mode")
+    }
+})
