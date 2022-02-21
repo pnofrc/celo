c144f217beb68be6b3ee40f937a8119a8ae08e0c
pnofrc <poni@riseup.net>
2022-02-21 01:20:16 +0100

    blackhole

--- cms.js
+++ cms.js
@@ -5,22 +5,22 @@ var assets = {
         'pic': ''
     },
     'ManuelRitz': {
-        'title': 'Manuel Ritz',
+        'title': 'MANUEL RITZ',
         'link': 'https://player.vimeo.com/video/352223101?h=043c2aac46&title=0&byline=0&portrait=0&sidedock=0',
         'pic': 'imag.gif'
     },
     'SimonaCorsellini': {
-        'title': 'Simona Corsellini',
+        'title': 'SIMONA CORSELLINI',
         'link': 'https://player.vimeo.com/video/400114006?h=043c2aac46&title=0&byline=0&portrait=0&sidedock=0',
         'pic': 'imag.gif'
     },
     'RogerVivier': {
-        'title': 'Roger Vivie',
+        'title': 'ROGER VIVIER',
         'link': 'https://player.vimeo.com/video/481229528?h=043c2aac46&title=0&byline=0&portrait=0&sidedock=0',
         'pic': 'imag.gif'
     },
     'Coltorti': {
-        'title': 'Coltorti',
+        'title': 'COLTORTI',
         'link': 'https://player.vimeo.com/video/352222024?h=043c2aac46&title=0&byline=0&portrait=0&sidedock=0',
         'pic': 'imag.gif'
     },
@@ -31,17 +31,17 @@ var assets = {
 
     },
     'Nike-Yoox': {
-        'title': 'Nike/Yoox',
+        'title': 'NIKE YOOX',
         'link': 'https://player.vimeo.com/video/496674956?h=043c2aac46&title=0&byline=0&portrait=0&sidedock=0&sidedock=0',
         'pic': 'imag.gif'
     },
     'Womsh': {
-        'title': 'Womsh',
+        'title': 'WOMSH',
         'link': 'https://player.vimeo.com/video/569872956?h=043c2aac46&title=0&byline=0&portrait=0&sidedock=0',
         'pic': 'imag.gif'
     },
     'Womsh2': {
-        'title': 'Womsh2',
+        'title': 'WOMSH2',
         'link': 'https://player.vimeo.com/video/569872956?h=043c2aac46&title=0&byline=0&portrait=0&sidedock=0',
         'pic': 'imag.gif'
     }
