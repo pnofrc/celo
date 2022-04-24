171f9d42b4967096f0ea64a13d9da120a396d1b3
pnofrc <poni@riseup.net>
2022-04-24 23:19:55 +0200

    new remains assets

--- assets/remains/fisheye.sh
+++
@@ -1,4 +0,0 @@
-    for i in *.png
-       do /Users/funix/celo/lab/ffmpeg -i $x -filter:v "crop=950:950"  "$i"
-sh /Users/funix/celo/lab/fisheye -o 90 -i 90 -t linear -f circular -m transparent "$i" "${i%.*}.png"
- done
